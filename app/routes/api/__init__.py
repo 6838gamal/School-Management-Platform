ecb06c9c27003e58fe02affec0cdd13f27e4a622
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-08-26 22:19:38 +0300

    Update __init__.py

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,1 +1,14 @@
 """API v1 routes package."""
+
+
+"""API routes."""
+from fastapi import APIRouter
+
+from app.routes.api import v1
+
+router = APIRouter(prefix="/api")
+
+# تسجيل جميع إصدارات API
+router.include_router(v1.router, prefix="/v1")
+
+__all__ = ["router"]
