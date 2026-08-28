391860c759dd16cf03c5af28a61800f7d1579b72
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-08-28 19:51:23 +0300

    Update __init__.py

--- app/routes/web/__init__.py
+++ app/routes/web/__init__.py
@@ -2,4 +2,16 @@
 # Makes web a Python package
 from app.routes.web.teachers import router as teachers_router
 
-__all__ = ['teachers_router']
+
+
+from fastapi import APIRouter
+from app.routes.web import modules, grades
+
+router = APIRouter()
+
+# تسجيل الروات
+router.include_router(modules.router)
+router.include_router(grades.router)
+
+# يمكنك أيضاً تصدير router مباشرة
+__all__ = ["router","teachers_router"]
