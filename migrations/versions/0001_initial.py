c14fa66432f561e9c52dca8b8a5d6484a3e1a85d
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 17:20:31 +0300

    Update 0001_initial.py

--- migrations/versions/0001_initial.py
+++ migrations/versions/0001_initial.py
@@ -10,9 +10,9 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "0001"
-down_revision: None
-branch_labels: None
-depends_on: None
+down_revision: str | None = None
+branch_labels: str | None = None
+depends_on: str | None = None
 
 
 def upgrade() -> None:
