ca0ac28c0a7997c0987f25fc65638f1bbd0653f7
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 17:23:35 +0300

    Refactor migration identifiers to use type hints
    Update type hints for migration identifiers.

--- migrations/versions/0004_add_default_roles.py
+++ migrations/versions/0004_add_default_roles.py
@@ -14,9 +14,9 @@ from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
 revision: str = '0004'
-down_revision: '0003'
-branch_labels: None
-depends_on: None
+down_revision: str | None = '0003'
+branch_labels: str | None = None
+depends_on: str | None = None
 
 
 def upgrade() -> None:
