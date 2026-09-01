c3f1f4aabb94dc067afc41bf5ab4917a3082cfcd
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 16:37:44 +0300

    Rename migration file to 0003_seed_initial_data.py

--- migrations/versions/0003_seed_initial_data.py
+++ migrations/versions/0003_seed_initial_data.py
@@ -13,8 +13,8 @@ from sqlalchemy import text
 
 
 # revision identifiers, used by Alembic.
-revision: str = '002'
-down_revision: Union[str, None] = '001'
+revision: str = '0003'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
