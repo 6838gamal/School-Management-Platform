f24d91e2a2acf680ba231f8a4aa324fc677942ee
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 17:04:06 +0300

    Simplify migration version variables

--- migrations/versions/0001_initial.py
+++ migrations/versions/0001_initial.py
@@ -10,9 +10,9 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "0001"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: None
+branch_labels: None
+depends_on: None
 
 
 def upgrade() -> None:
