a3531d68d0198e8d3d7538ec1105b91b64d1c93b
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 16:38:32 +0300

    Update migration version to 0004 with no down_revision

--- migrations/versions/0004_add_default_roles.py
+++ migrations/versions/0004_add_default_roles.py
@@ -13,8 +13,8 @@ from sqlalchemy import text
 
 
 # revision identifiers, used by Alembic.
-revision: str = '003'
-down_revision: Union[str, None] = '002'
+revision: str = '0004'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
