b1edb98820b6f0f816461fa2ee604f15090f45cd
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-01 17:36:21 +0300

    Fix type annotations in migration script

--- migrations/versions/0010____20240831_0001_complete_schedule_upgrade.py
+++ migrations/versions/0010____20240831_0001_complete_schedule_upgrade.py
@@ -15,9 +15,9 @@ import uuid
 
 # revision identifiers, used by Alembic.
 revision = '0010'
-down_revision = str | None = '0009'
-branch_labels = str | None = None
-depends_on = str | None = None
+down_revision : str | None = '0009'
+branch_labels : str | None = None
+depends_on : str | None = None
 
 
 def upgrade() -> None:
